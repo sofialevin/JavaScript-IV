e1f9a61730686d3c786c37387c691b9dea95677b
sofialevin <slevin@M-4876.local>
2019-05-30 19:01:08 -0700

    Completed Instructor class in lambda-classes.js.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -15,3 +15,28 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
+
+// Now that we have a Person as our base class, we'll build our Instructor class.
+// Instructor uses the same attributes that have been set up by Person
+// Instructor has the following unique props:
+// specialty what the Instructor is good at i.e. 'redux'
+// favLanguage i.e. 'JavaScript, Python, Elm etc.'
+// catchPhrase i.e. Don't forget the homies
+// Instructor has the following methods:
+// demo receives a subject string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+// grade receives a student object and a subject string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+
+class Instructor extends Person {
+    constructor(attr) {
+        super(attr);
+        this.specialty = attr.specialty,
+        this.favLanguage = attr.favLanguage,
+        this.catchPhrase = attr.catchPhrase
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}.`
+    }
+    grade(student, subject) {
+        return `${student} receives a perfect score on ${subject}.`
+    }
+}
